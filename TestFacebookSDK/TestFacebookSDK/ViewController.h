66201456b92c368cdfa0dd05ff3bfff1ff068bc0
matsuda <hi@matsuda.me>
2013-06-03 12:15:40 +0900

    create project for test facebook-ios-sdk

---
+++ TestFacebookSDK/TestFacebookSDK/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestFacebookSDK
+//
+//  Created by Kosuke Matsuda on 2013/06/03.
+//  Copyright (c) 2013年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestFacebookSDK/TestFacebookSDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestFacebookSDK
+//
+//  Created by Kosuke Matsuda on 2013/06/03.
+//  Copyright (c) 2013年 matsuda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
