ff77d6495310e5ebc3e38c7093d43e0165cd1b92
matsuda <hi@matsuda.me>
2013-06-06 10:34:22 +0900

    各種frameworkを追加し、shareができるように

--- TestFacebookSDK/TestFacebookSDK/ViewController.m
+++ TestFacebookSDK/TestFacebookSDK/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
 
 @interface ViewController ()
 
@@ -24,6 +25,39 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)share:(id)sender
+{
+    NSURL *url = [NSURL URLWithString:@"http://www.dhc.co.jp/goods/goodsdetail.jsp?gCode=22504"];
+    FBAppCall *result = [FBDialogs presentShareDialogWithLink:url
+//                                                       name:@"test share"
+                                                    handler:^(FBAppCall *call, NSDictionary *results, NSError *error) {
+        if(error) {
+            NSLog(@"Error: %@", error.description);
+        } else {
+            NSLog(@"Success!");
+        }
+    }];
+    NSLog(@"cccccccccc >>>>>>>>>> %@", result);
+    if (!result) {
+//        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"share" message:@"can't share" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+//        [alert show];
+//        NSDictionary *params = @{@"naem": @"FBTest",
+//                                 @"caption": @"caption test",
+//                                 @"description": @"test ios SDK",
+//                                 @"link": @"http://www.dhc.co.jp/goods/goodsdetail.jsp?gCode=22504",
+//                                 @"image": @"http://www.dhc.co.jp/goods/4/22504_L.jpg"};
+//        [FBWebDialogs presentFeedDialogModallyWithSession:nil parameters:params handler:^(FBWebDialogResult result, NSURL *resultURL, NSError *error) {
+//            NSLog(@"result >>>>> %d", result);
+//            NSLog(@"resultURL >>> %@", resultURL);
+//            if(error) {
+//                NSLog(@"Error: %@", error.description);
+//            } else {
+//                NSLog(@"Success!");
+//            }
+//        }];
+    }
 }
 
 @end
