6ae06b467ca3046fe83c7976943d9bdf97d416bd
matsuda <hi@matsuda.me>
2013-06-26 19:02:15 +0900

    create project to test XSWI

---
+++ TestXSWI/TestXSWI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestXSWI
+//
+//  Created by Kosuke Matsuda on 2013/06/26.
+//  Copyright (c) 2013年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
