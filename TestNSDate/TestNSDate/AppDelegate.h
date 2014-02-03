d2630184f9d4e4be6283af4906c28167ce2b6004
matsuda <3matsuda@gmail.com>
2014-02-03 10:18:27 +0900

    create project for test NSDate

---
+++ TestNSDate/TestNSDate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestNSDate
+//
+//  Created by Kosuke Matsuda on 2013/07/12.
+//  Copyright (c) 2013年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
