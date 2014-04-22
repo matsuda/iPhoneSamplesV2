87398d84e4c7f2d6db1f75747389d93f0908dd1c
Kosuke Matsuda <3matsuda@gmail.com>
2014-04-22 16:35:19 +0900

    create project for sqlcipher

---
+++ TestSQLCipher/TestSQLCipher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSQLCipher
+//
+//  Created by Kosuke Matsuda on 2014/04/22.
+//  Copyright (c) 2014年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestSQLCipher/TestSQLCipher/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestSQLCipher
+//
+//  Created by Kosuke Matsuda on 2014/04/22.
+//  Copyright (c) 2014年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
