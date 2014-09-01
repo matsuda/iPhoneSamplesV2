d0c0c5ebd42c42b92b4736a4e283932192e107e6
matsuda <3matsuda@gmail.com>
2014-09-01 17:45:44 +0900

    NSURLSessionを試す

--- TestBackgroundTask/TestBackgroundTask/ViewController.m
+++ TestBackgroundTask/TestBackgroundTask/ViewController.m
@@ -8,8 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <NSURLSessionDownloadDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
 @implementation ViewController
@@ -17,11 +17,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    APPLog();
+    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)tapButton:(id)sender
+{
+    [self startSession];
+}
+
+- (void)startSession
+{
+    NSString *identifier = @"BackgroundSessionConfiguration";
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration backgroundSessionConfiguration:identifier];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
+
+    NSURL *url = [NSURL URLWithString:@"http://www.nitteleplus.com/program/anime/photo/mezon_ikkoku.jpg"];
+    NSURLSessionDownloadTask *task = [session downloadTaskWithURL:url];
+    [task resume];
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didResumeAtOffset:(int64_t)fileOffset expectedTotalBytes:(int64_t)expectedTotalBytes
+{
+    APPLog();
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
+{
+    APPLog();
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location
+{
+    APPLog(@"location >>>>> %@", location);
+    NSData *data = [NSData dataWithContentsOfURL:location];
+    UIImage *image = [UIImage imageWithData:data];
+    if (image) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.imageView.image = image;
+        });
+    }
+}
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
+{
+    APPLog();
+    if (error) {
+        NSLog(@"error >>>>> %@", error);
+    }
+    [session invalidateAndCancel];
 }
 
 @end
